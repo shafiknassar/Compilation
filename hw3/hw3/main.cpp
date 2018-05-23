f17fb2008c5f3eb4babbfb3eb8df7bbd5365fe81
shafiknassar <shafiknassar@campus.technion.ac.il>
2018-05-23 15:46:30 +0300

    hw3 started

---
+++ hw3/hw3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hw3
+//
+//  Created by Shafik Nassar on 23/05/2018.
+//  Copyright © 2018 ShafikNassar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
