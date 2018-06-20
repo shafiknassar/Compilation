919126108531239b518763f33f3905ff26c2b01b
BilalTamish <b.tamish@campus.technion.ac.il>
2018-06-20 20:31:04 +0300

    registers

--- hw5/hw5/hw5/RegistersPool.cpp
+++ hw5/hw5/hw5/RegistersPool.cpp
@@ -7,6 +7,7 @@
 //
 
 #include "includes.h"
+#define not_found "not found"
 using std::vector;
 using std::string;
 
@@ -15,34 +16,53 @@ void Register::free() {
     inUse = false;
 }
 
-Register* MipsRegisters::getValue(int registerName) {
-    return registers[registerName];
+MipsRegisters::MipsRegisters() {
+    // 0=>$zero; 1=>$at; 2,3=>$v0,$v1; 4-7=>$a0-$a3; 8-15=>$t0-$t7;
+    // 16-23=>$s0-$s7; 24,25=>$t8,$t9; 26,27=>$k0,$k1;
+    // 28=>$gp; 29=>$sp; 30=>$fp; 31=>$ra;
+    string reg_name[] = {"$zero", "$at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3",
+                     "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7",
+                     "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7",
+                     "$t8", "$t9", "$k0", "$k1", "$gp", "$sp", "$fp", "$ra"};
+    for (int i = 0; i < REGISTERS_NUM; i++) {
+        registers[reg_name[i]] = Register();
+    }
+    pc = 4;
 }
 
-void MipsRegisters::Bind(int registerName, string var) {
+string MipsRegisters::getVarName(string registerName) {
+    return registers[registerName].varName;
+}
+
+void MipsRegisters::Bind(string registerName, string var) {
     // can't use $zero
-    if (registerName == 0 || registers[registerName]->inUse) return;
-    Register* reg = registers[registerName];
-    reg->varName = var;
-    reg->inUse = true;
+    if (registerName == "$zero" || registers[registerName].inUse) return;
+    Register& reg = registers[registerName];
+    reg.varName = var;
+    reg.inUse = true;
 }
 
-void MipsRegisters::clearRegisters() {
-    registers.clear();
-    registers.resize(REGISTERS_NUM);
+void MipsRegisters::unbind(string registerName) {
+    Register& reg = registers[registerName];
+    if (registerName == "$zero" || !reg.inUse) return;
+    reg.free();
 }
 
-int MipsRegisters::registerLookup(string varName) {
-    for (int i = 0 ; i < REGISTERS_NUM; i++) {
-        if (registers[i]->varName == varName) return i;
+string MipsRegisters::registerLookup(string varName) {
+    map<string, Register>::iterator it;
+    for (it = registers.begin(); it != registers.end(); ++it) {
+        Register& reg = (*it).second;
+        if (reg.varName == varName) return (*it).first;
     }
-    return -1;
+    return not_found;
 }
 
-int MipsRegisters::getEmptyRegister() {
+string MipsRegisters::getEmptyRegister() {
     //we can use: $t0-$t7, $s0-$s4, $t8-$t9
-    for (int i =  8; i <= 25; i++) {
-        if (!registers[i]->inUse) return i;
+    map<string, Register>::iterator it;
+    for (it = registers.begin(); it != registers.end(); ++it) {
+        Register& reg = (*it).second;
+        if (!reg.inUse) return (*it).first;
     }
-    return -1;
+    return not_found;
 }
--- hw5/hw5/hw5/RegistersPool.hpp
+++ hw5/hw5/hw5/RegistersPool.hpp
@@ -15,6 +15,7 @@
 
 using std::string;
 using std::vector;
+using std::map;
 
 struct Register {
     string varName;
@@ -30,15 +31,15 @@ struct MipsRegisters {
     // 0=>$zero; 1=>$at; 2,3=>$v0,$v1; 4-7=>$a0-$a3; 8-15=>$t0-$t7;
     // 16-23=>$s0-$s7; 24,25=>$t8,$t9; 26,27=>$k0,$k1;
     // 28=>$gp; 29=>$sp; 30=>$fp; 31=>$ra;
-    vector<Register*> registers; //available registers
+    map<string, Register> registers; //available registers
     int pc; //program counter
     
-    MipsRegisters(): registers(REGISTERS_NUM), pc(WORD_SIZE) {}
-    Register* getValue(int registerName);
-    void Bind(int registerName, string var);
-    void clearRegisters();
-    int registerLookup(string varName);
-    int getEmptyRegister();
+    MipsRegisters();
+    void Bind(string registerName, string var);
+    void unbind(string registerName);
+    string getVarName(string registerName);
+    string registerLookup(string varName);
+    string getEmptyRegister();
     
 };
 #endif /* RegisterPool_hpp */
