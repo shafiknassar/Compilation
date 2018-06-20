014da9bdafda7eb0b59c7f1a866077a4654fc95f
shafiknassar <shafiknassar@campus.technion.ac.il>
2018-06-20 17:52:07 +0300

    Assembler 2

--- hw5/hw5/hw5/Assembler.hpp
+++ hw5/hw5/hw5/Assembler.hpp
@@ -16,10 +16,43 @@ using std::string;
 
 class Assembler {
 private:
-    CodeBuffer codeBuff;
+    CodeBuffer &codeBuff;
+    int stringDataCounter;
 public:
-    int emitCode(string code);
     
+    Assembler() :
+        codeBuff(CodeBuffer::instance()),
+        stringDataCounter(0)
+    {
+        
+    }
+    /*
+     @description: emits code to text section.
+     @returns the location in code buffer.
+     */
+    int emitCode(string code);
+    /* 
+     @description: adds a string literal to the data section.
+     @returns the label assigned to string.
+     */
+    string addStringLiteral(string value);
+    
+    /*
+     @description: generates a label for the next intsruction
+     @returns the label assigned.
+     */
+    string getNextInst();
+    /*
+     @description: generates code for handling division by zero exception
+     @returns none.
+     */
+    void emitDivByZero(); /* TODO: implement */
+    
+    /*
+     @description: generates code binary operation
+     @returns none.
+     */
+    void emitBinOp(string op, string trgPlace, string src1Place, string src2Place);
 };
 
 #endif /* Assembler_hpp */
